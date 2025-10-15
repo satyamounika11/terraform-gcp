89ffcc63ee19d4dbb0667665839fe0659be2e1b4
satyamounika11 <37068004+satyamounika11@users.noreply.github.com>
2025-10-15 19:55:51 +0530

    Update the condition with try that supports present version

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,5 @@
-# Output the external IP if the VM is created
 output "vm_external_ip" {
   description = "External IP address of the created VM (if any)"
-  value       = google_compute_instance.micro_vm[0].network_interface[0].access_config[0].nat_ip
-  condition   = var.enable_ec2_instance
+  value       = try(google_compute_instance.micro_vm[0].network_interface[0].access_config[0].nat_ip, null)
 }
 
