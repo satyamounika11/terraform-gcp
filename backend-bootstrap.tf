85053c0debf18dd5ec4a1a1048bf17080ec4d21b
satyamounika11 <37068004+satyamounika11@users.noreply.github.com>
2025-10-30 00:17:20 +0530

    remove backend bootstrap (no longer needed)

--- backend-bootstrap.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "terraform_state" {
-  name          = "sandbox-tf-state-443817"
-  location      = var.region
-  force_destroy = true # Allows cleanup if bucket is empty later
-}
-
